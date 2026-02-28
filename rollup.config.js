e441d0be203bf21974d153c559bc8df7926ccf66
acamposuribe <alejandro@arqtisti.com>
2026-02-28 17:04:12 +0100

    iFrame pixeldensity fix?

--- rollup.config.js
+++ rollup.config.js
@@ -29,6 +29,7 @@ const config = {
 `<html>
   <head> 
     <title>${title}</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <style>
         *, *::before, *::after { box-sizing: border-box; }
         html, body { margin: 0; padding: 0; width: 100%; height: 100%; overflow: hidden; }
